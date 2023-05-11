533c465f15246d8afe66a79e5128734ecf8f6d35
alfirosyadi <muhammadalfirosyadi1998@gmail.com>
2023-05-11 19:07:49 +0700

    Manage List of Users via state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,8 +9,16 @@ const AddUser = props => {
     const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
-        event.preventDefault();  
-        console.log(enteredUsername, enteredAge);
+        event.preventDefault();
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return
+        }
+        if(+enteredAge < 1) {
+            return
+        }
+        props.onAddUser(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
     const usernameChangeHandler = (event) => {
@@ -25,9 +33,9 @@ const AddUser = props => {
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}></input>
+                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}></input>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler}></input>
+                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
